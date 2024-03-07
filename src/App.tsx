20ef6ee5d8a338245119ac711101382ff18ab3b5
anon36318 <anon36318@gmail.com>
2024-03-07 19:01:13 +0900

    contents added

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import TextBox from "./TextBox";
+import { WindowSizeContextProvider } from "./contexts/WindowSizeContext";
+import Markdown from "./Markdown";
+import AbstractBox from "./AbstractBox";
+import {
+  bgColor,
+  tldr,
+  abstract,
+
+} from "./const";
+
+function App() {
+  return (
+    <>
+      <WindowSizeContextProvider>
+        <div className="w-screen h-screen min-w-[1400px]">
+          {/* FIXME: Title, Author */}
+          <TextBox
+            title={
+              <>
+                Improving Diffusion Models for
+                <br />
+                Authentic Virtual Try-on in the Wild
+              </>
+            }
+            backgroundColor={"white"}
+            textColor={"black"}
+            titleFontSize={"text-5xl"}
+          >
+          </TextBox>
+          {/* FIXME: TL;DR */}
+          <TextBox
+            title={`TL;DR: ${tldr}`}
+            backgroundColor={bgColor}
+            textColor={"white"}
+            titleFontSize={"text-3xl"}
+          />
+          {/* FIXME: Abstract */}
+          <AbstractBox
+            title={"Abstract"}
+            backgroundColor={"white"}
+            textColor={"black"}
+          >
+            <Markdown text={abstract} />
+          </AbstractBox>
+
+          <TextBox
+            title={"ECCV 2024 Submission"}
+            backgroundColor={bgColor}
+            textColor={"white"}
+          />
+        </div>
+      </WindowSizeContextProvider>
+    </>
+  );
+}
+
+export default App;
